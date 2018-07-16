c275719a88e27e668302d8c76a788e1e6b2de43b
siridzava <butters@ukr.net>
2018-07-16 07:15:46 +0300

    added upadate

--- level_cbv/cbv/views.py
+++ level_cbv/cbv/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return render(request, 'index.html')
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'some random text'
+        return context
