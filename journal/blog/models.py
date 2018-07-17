748ca81a68b9bffeb8b1a50d2a43d48aba4facbd
siridzava <butters@ukr.net>
2018-07-17 22:58:07 +0300

    added blog

--- journal/blog/models.py
+++ journal/blog/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -26,7 +26,8 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Pot', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments',
+                             on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
