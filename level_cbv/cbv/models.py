255602b8cb6ddf06677dd43e7fb9d7c3a7f4f663
Yuriy Gordon-Basanskiy <y_gordon-basanskiy@wargaming.net>
2018-07-17 11:26:02 +0300

    cbv is done

--- level_cbv/cbv/models.py
+++ level_cbv/cbv/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -11,11 +13,14 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("cbv:detail", kwargs={'pk': self.pk})
+
 
 class Student(models.Model):
     name = models.CharField(max_length=256)
     age = models.PositiveIntegerField()
-    school = models.ForeignKey(School, related_name='students', on_delete='CASCADE')
+    school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
